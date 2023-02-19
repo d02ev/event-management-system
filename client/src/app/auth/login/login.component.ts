2ef5426828e016125605721c44be83d31c010fb5
d02ev <vikramadityapratapsingh98@gmail.com>
2023-02-19 18:45:40 +0530

    add auth component

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public loginForm: any;
+  public isRequestComplete: boolean = false;
+  public isError: boolean = false;
+  public errorMessage: string = '';
 
+  constructor(private _authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  onSubmit() {
+    this._authService.login(this.loginForm.value).subscribe({
+      next: (response: any) => {
+        this.isRequestComplete = true;
+        localStorage.setItem('authToken', response.jwtToken);
+      },
+      error: (err: any) => {
+        this.isError = true;
+
+        if (err.error.status == 404) {
+          this.errorMessage = 'User Does Not Exist';
+        } else {
+          this.errorMessage = 'Internal Server Error!';
+        }
+
+        setTimeout(() => {
+          this.isError = false;
+        }, 3000);
+      },
+      complete: () => {},
+    });
+  }
 }
--- client/src/app/auth/register/register.component.ts
+++ client/src/app/auth/register/register.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  public registrationForm: any;
+  public isRequestComplete: boolean = false;
+  public isError: boolean = false;
+  public errorMessage: string = '';
 
+  constructor(private _authService: AuthService, private _router: Router) {}
+
+  ngOnInit(): void {
+    this.registrationForm = new FormGroup({
+      fullName: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  onSubmit() {
+    this._authService.register(this.registrationForm.value).subscribe({
+      next: (response: any) => {
+        this.isRequestComplete = true;
+      },
+      error: (err: any) => {
+        this.isError = true;
+
+        if (err.error.status == 400) {
+          this.errorMessage = 'User Already Exists!';
+        } else {
+          this.errorMessage = 'Internal Server Error!';
+        }
+
+        setTimeout(() => {
+          this.isError = false;
+        }, 3000);
+      },
+      complete: () => {
+        setTimeout(() => {
+          this._router.navigate(['/auth/login']);
+        }, 3000);
+      },
+    });
+  }
 }
