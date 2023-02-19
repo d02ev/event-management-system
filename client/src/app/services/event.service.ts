5761dd0bea1a3d91aa74e7a8637b652e209f6725
d02ev <vikramadityapratapsingh98@gmail.com>
2023-02-19 18:44:00 +0530

    add event services

---
+++ client/src/app/services/event.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+  private _baseUrl: string = 'http://localhost:5000/api/v1/event'
+
+  constructor(private _httpClient: HttpClient) { }
+
+  // create events
+  public createEvent(creationData: any): Observable<any> {
+    return this._httpClient.post(this._baseUrl, creationData);
+  };
+
+  // access all events
+  public accessAllEvents(): Observable<any> {
+    return this._httpClient.get(this._baseUrl);
+  }
+
+  // access all public events
+  public accessAllPublicEvents(): Observable<any> {
+    return this._httpClient.get(this._baseUrl + '/public');
+  }
+
+  // access user created events
+  public accessUserCreatedEvents(): Observable<any> {
+    return this._httpClient.get(this._baseUrl + '/dashboard');
+  }
+
+  // edit event
+  public modifyEvent(eventId: any, modificationData: any): Observable<any> {
+    return this._httpClient.patch(`${this._baseUrl}/edit/${eventId}`, modificationData);
+  }
+
+  // delete event
+  public deleteEvent(eventId: any): Observable<any> {
+    return this._httpClient.delete(`${this._baseUrl}/delete/${eventId}`);
+  }
+}
