8a09fc96f8d8b68b684836a21150ccb3a54a2712
d02ev <vikramadityapratapsingh98@gmail.com>
2023-02-20 13:21:45 +0530

    update default home component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -5,21 +5,29 @@ import { EventService } from '../services/event.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  public events: any;
+  public futureEvents: any = [];
+  public pastEvents: any = [];
 
   constructor(private _eventService: EventService) {}
 
   ngOnInit(): void {
-      this._eventService.accessAllPublicEvents().subscribe({
-        next: (response: any) => {
-          console.log(response);
-        },
-        error: (err: any) => { },
-        complete: () => { }
-      });
+    this._eventService.accessAllPublicEvents().subscribe({
+      next: (response: any) => {
+        response.forEach((event: any) => {
+          if (new Date() < new Date(event.eventDate + ' ' + event.eventTime)) {
+            this.futureEvents.push(event);
+          } else if (
+            new Date() > new Date(event.eventDate + ' ' + event.eventTime)
+          ) {
+            this.pastEvents.push(event);
+          }
+        });
+      },
+      error: (err: any) => {},
+      complete: () => {},
+    });
   }
-
 }
